67064c49a08e4ba2e3f99f2a5c8df53edc038c9a
Nagaraj <raviteja.xworkz@gmail.com>
2022-10-12 23:54:14 +0530

    Devuduu

---
+++ teashop/src/com/xworkz/teashop/boot/CigaretteRunner.java
@@ -0,0 +1,45 @@
+package com.xworkz.teashop.boot;
+
+import com.xworkz.teashop.Cigarette;
+
+public class CigaretteRunner {
+
+	public static void main(String[] args) {
+			
+				Cigarette cigrette =new Cigarette();
+				
+				cigrette.name="gold flake";
+				cigrette.adv="stress relief";
+				cigrette.dis="lung cancer";
+				cigrette.effect="mental illness";
+				cigrette.size="10cm";
+				cigrette.price=10;
+				cigrette.colour="white and brown";
+				cigrette.shape="round";
+				cigrette.healthy=false;
+				System.out.println("cigrette name  :"+cigrette.name);
+				System.out.println("cigrette adv  :"+cigrette.adv);
+				System.out.println("cigrette dis  :"+cigrette.dis);
+				System.out.println("cigrette effect :"+cigrette.effect);
+				System.out.println("cigrette size  :"+cigrette.size);
+				System.out.println("cigrette price :"+cigrette.price);
+				System.out.println("cigrette colour  :"+cigrette.colour);
+				System.out.println("cigrette shape :"+cigrette.shape);
+				System.out.println("cigrette is healthy for body :"+cigrette.healthy);
+				System.out.println("cigrette has chemical  :"+cigrette.chemical);
+				System.out.println("cigrette is a killer :"+cigrette.itIsKiller);
+				System.out.println("cigrette has gases like :"+cigrette.gas);
+				System.out.println("to burn :"+cigrette.time);
+				System.out.println("good fact  :"+cigrette.fact);
+				System.out.println("it affect fertility :"+cigrette.itAffectFertility);
+				System.out.println("it is god for health  :"+cigrette.itIsGoodForHealth);
+				System.out.println("it causes mental disorder  :"+cigrette.mentalDisorder);
+				System.out.println("it gives temp relief :"+cigrette.tempRelief);
+				
+				
+				
+			}
+		
+	}
+
+
